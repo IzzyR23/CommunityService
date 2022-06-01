00b8df1f7d78449572d56449c0b887933e8e104a
IzzyR23 <106546558+IzzyR23@users.noreply.github.com>
2022-06-01 16:47:14 +0100

    Add files via upload

---
+++ scripts/videoend.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class videoend : MonoBehaviour
+{
+
+    VideoPlayer video;
+    
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
